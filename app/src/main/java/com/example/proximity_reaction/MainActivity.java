08585ddc0c3531e3b9a59b070b27fc24f3d52f24
AnasChebili <125923527+AnasChebili@users.noreply.github.com>
2025-03-09 12:20:09 +0100

    implemented initial UI

--- app/src/main/java/com/example/proximity_reaction/MainActivity.java
+++ app/src/main/java/com/example/proximity_reaction/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.proximity_reaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView display;
+    private Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+        start = findViewById(R.id.start);
+        start.setOnClickListener(v -> display.setText("Button Clicked!"));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
